43e579101791cd95ce866f8097834db17a1e45ad
JuhoKang <rjduakcjs@gmail.com>
2018-01-25 09:11:49 +0900

    in process of refactoring socket.js, add health feature of chunks

--- config/socket.js
+++ config/socket.js
@@ -6,20 +6,36 @@ const paintChunkController = require('../controllers/paintChunkController');
 const fabric = require('fabric').fabric;
 const fs = require('fs');
 
+const MAX_CHUNK_HEALTH = 3;
+
 module.exports = function (server) {
   const chunks = {};
+  const chunkHealth = {};
+  const chunkInterval = {};
+
   function updateChunk(x, y) {
-    console.log(`update chunk ${x},${y}`);
+    console.log(`update chunk ${x},${y} : chunkHealth is : ${chunkHealth[`${x},${y}`]}`);
     const target = chunks[`${x},${y}`];
     paintChunkController.paintchunk_save(x, y, JSON.stringify(target));
+    if(chunkHealth[`${x},${y}`] < 0) {
+      chunks[`${x},${y}`] = null;
+      clearInterval(chunkInterval[`${x},${y}`]);
+      chunkInterval[`${x},${y}`] = null;
+    } else {
+      console.log(`hello`);
+      chunkHealth[`${x},${y}`] = chunkHealth[`${x},${y}`] - 1;
+    }
   }
+
   let io = require('socket.io')(server);
 
   io.on('connection', function (socket) {
     
     socket.on('getChunkData', (data) => {
+      //no chunk hit on memory
       if (chunks[`${data.x},${data.y}`] == null) {
         chunks[`${data.x},${data.y}`] = fabric.createCanvasForNode(4096, 4096);
+        //get data from database
         PaintChunk.findOne({
           x_axis: data.x,
           y_axis: data.y,
@@ -33,15 +49,20 @@ module.exports = function (server) {
               socket.emit('otherChunkSend', { x: data.x, y: data.y, json: JSON.stringify(chunks[`${data.x},${data.y}`]) });
             }
           } else {
-            console.log('chunk is null');
+            //no chunk data from database
+            //console.log('chunk is null');
           }
         }, (err) => {
           // need better error handling
           debug(err);
         });
-        setInterval(() => {
+        //update intialized chunk after 30sec
+        
+        chunkHealth[`${data.x},${data.y}`] = MAX_CHUNK_HEALTH;
+        const interval = setInterval(() => {
           updateChunk(data.x, data.y);
-        }, 15000);
+        }, 3000);        
+        chunkInterval[`${data.x},${data.y}`] = interval;
       } else {
         if (data.isMain) {
           socket.emit('mainChunkSend', { x: data.x, y: data.y, json: JSON.stringify(chunks[`${data.x},${data.y}`]) });
@@ -53,22 +74,17 @@ module.exports = function (server) {
 
     socket.on('joinRoom', (coord) => {
       socket.join(`chunk_room:${coord.x},${coord.y}`);
-      //for (let i = 0; i < data.length; i += 1) {
-      //  socket.join(`paint_room:${data[i]}`);
-      // console.log(`user is joining : paint_room:${data[i]}`);
-      //}
-      // console.log(io.sockets.adapter.rooms);
     });
 
     socket.on('drawToRoom', (data) => {
-      // console.log(data.coord);
       socket.broadcast.to(`paint_room:${data.coord}`).emit('drawing', data.drawData);
     });
 
     socket.on('drawToChunk', (data) => {
-
+      //no chunk hit on memory
       if (chunks[`${data.xAxis},${data.yAxis}`] == null) {
-        console.log(`${data.xAxis},${data.yAxis} : is null`);
+        //console.log(`${data.xAxis},${data.yAxis} : is null`);
+        MAX_CHUNK_HEALTH
         chunks[`${data.xAxis},${data.yAxis}`] = fabric.createCanvasForNode(4096, 4096);
         
         PaintChunk.findOne({
@@ -92,10 +108,16 @@ module.exports = function (server) {
           // need better error handling
           debug(err);
         });
-        setInterval(() => {
+        
+        chunkHealth[`${data.x},${data.y}`] = MAX_CHUNK_HEALTH;
+        const interval = setInterval(() => {
           updateChunk(data.xAxis, data.yAxis);
-        }, 360000);
+        }, 30000);
+        chunkInterval[`${data.x},${data.y}`] = interval;
+        
       } else {
+        chunkHealth[`${data.x},${data.y}`] = MAX_CHUNK_HEALTH;
+
         fabric.util.enlivenObjects([data.data], (objects) => {
           objects.forEach((obj) => {
             obj.left = data.serverLeft;
